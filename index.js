7c6c9ab92992c9c0484df29edbb725abd3f9885b
xurichard <richardxu@berkeley.edu>
2016-08-04 14:18:39 -0700

    added pictures and 33% width

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // external js: masonry.pkgd.js
-
 var $grid = $('.grid').masonry({
   columnWidth: 160,
   itemSelector: '.grid-item'
@@ -7,18 +6,33 @@ var $grid = $('.grid').masonry({
 
 var photosets = []
 
-var flickerAPI = "https://api.flickr.com/services/rest/?&method=flickr.photosets.getList&api_key=41dd3aff041c00c52febdef9786a9ca0&user_id=139169754@N02&format=json&nojsoncallback=1&per_page=10&privacy_filter=1";
+var flickerAPI = "https://api.flickr.com/services/rest/?&method=flickr.photosets.getList&api_key=41dd3aff041c00c52febdef9786a9ca0&user_id=139169754@N02&format=json&nojsoncallback=1&privacy_filter=1";
 $.getJSON(flickerAPI, {
   format: "json"
 })
 .done(function( data ) {
   photosets = data.photosets.photoset
+  $.each( photosets, function(i, photoset){
+    getphotos(photoset)
+  })
 });
 
-for(i = 0; i < photosets.length; i ++ ){
-  var p = photos[i]
-  var url = "https://farm" + p.farm + ".staticflickr.com/" + p.server + "/" + p.id + "_" + p.secret + "_z.jpg"
-}
+
+var all_photos = []
+var all_photos_index = 0
+
+var getphotos = function( photoset ){
+  call = "https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=41dd3aff041c00c52febdef9786a9ca0&user_id=139169754@N02&format=json&nojsoncallback=1&privacy_filter=1&photoset_id=" + photoset.id;
+  $.getJSON(call, {
+    format: "json"
+  })
+  .done(function( data ) {
+    $.each( data.photoset.photo, function( i, p ) {
+      var url = "https://farm" + p.farm + ".staticflickr.com/" + p.server + "/" + p.id + "_" + p.secret + "_z.jpg"
+      all_photos.push(url)
+    })
+  })
+};
 
 // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 // $.getJSON( flickerAPI, {
@@ -39,13 +53,12 @@ for(i = 0; i < photosets.length; i ++ ){
 
 window.onscroll = function(ev) {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        var elems = [ getItemElement(), getItemElement(), getItemElement() ];
-    // make jQuery object
-    var $elems = $( elems );
-    $grid.append( $elems ).masonry( 'appended', $elems );
+      var elems = [ getItemElement(), getItemElement(), getItemElement() ];
+      // make jQuery object
+      var $elems = $( elems );
+      $grid.append( $elems ).masonry( 'appended', $elems );
     }
 };
-
 
 $('.append-button').on( 'click', function() {
   var elems = [ getItemElement(), getItemElement(), getItemElement() ];
@@ -57,10 +70,10 @@ $('.append-button').on( 'click', function() {
 // create <div class="grid-item"></div>
 function getItemElement() {
   var elem = document.createElement('div');
-  var wRand = Math.random();
-  var hRand = Math.random();
-  var widthClass = wRand > 0.8 ? 'grid-item--width3' : wRand > 0.6 ? 'grid-item--width2' : '';
-  var heightClass = hRand > 0.85 ? 'grid-item--height4' : hRand > 0.6 ? 'grid-item--height3' : hRand > 0.35 ? 'grid-item--height2' : '';
-  elem.className = 'grid-item ' + widthClass + ' ' + heightClass;
+  elem.className = 'grid-item ';
+  var img = document.createElement("img");
+  img.setAttribute("src", all_photos[all_photos_index]);
+  all_photos_index += 1;
+  elem.appendChild(img)
   return elem;
 }
