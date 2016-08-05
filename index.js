fc9d88eaf0279c5712370d8b885a6e04a3444b2c
richard.xu <richard.xu@tubemogul.com>
2016-08-04 17:40:00 -0700

    only refits photos when resize window

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ var getphotos = function( photoset ){
 // create <div class="grid-item"></div>
 function getItemElement() {
   var elem = document.createElement('div');
-  elem.className = 'grid-item ';
+  elem.className = 'item ';
   var img = document.createElement("img");
   img.setAttribute("src", all_photos[all_photos_index]);
   all_photos_index += 1;
@@ -58,42 +58,32 @@ function getItems(){
   return $( [getItemElement(), getItemElement(), getItemElement()])
 }
 
-
 $( function() {
 
-  var $grid = $('#grid').masonry({
+  var $container = $('#container').masonry({
     itemSelector: '.item',
     fitWidth: true,
   });
 
   $('#load-images').click( function() {
     var $items = getItems();
-    $grid.append( $items ).masonry( 'appended', $items );
-
-    // // hide by default
-    // $items.hide();
-    // // append to container
-    // $container.append( $items );
-    // $items.imagesLoaded().progress( function( imgLoad, image ) {
-    //   // get item
-    //   // image is imagesLoaded class, not <img>
-    //   // <img> is image.img
-    //   var $item = $( image.img ).parents('.item');
-    //   // un-hide item
-    //   $item.show();
-    //   // masonry does its thing
-    //   $container.masonry( 'appended', $item );
-    // });
+    // hide by default
+    $items.hide();
+    // append to container
+    $container.append( $items );
+    $items.imagesLoaded().progress( function( imgLoad, image ) {
+      // get item
+      // image is imagesLoaded class, not <img>
+      // <img> is image.img
+      var $item = $( image.img ).parents('.item');
+      // un-hide item
+      $item.show();
+      // masonry does its thing
+      $container.masonry( 'appended', $item );
+    });
   });
   
 });
-
-
-
-
-
-
-
 
 
 // window.onscroll = function(ev) {
@@ -111,3 +101,70 @@ $( function() {
 //   var $elems = $( elems );
 //   $grid.append( $elems ).masonry( 'appended', $elems );
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EXAMPLE
+
+
+// $( function() {
+
+//   var $container = $('#container').masonry({
+//     itemSelector: '.item',
+//     columnWidth: 200
+//   });
+
+//   $('#load-images').click( function() {
+//     var $items = getItems();
+//     // hide by default
+//     $items.hide();
+//     // append to container
+//     $container.append( $items );
+//     $items.imagesLoaded().progress( function( imgLoad, image ) {
+//       // get item
+//       // image is imagesLoaded class, not <img>
+//       // <img> is image.img
+//       var $item = $( image.img ).parents('.item');
+//       // un-hide item
+//       $item.show();
+//       // masonry does its thing
+//       $container.masonry( 'appended', $item );
+//     });
+//   });
+  
+// });
+
+// function randomInt( min, max ) {
+//   return Math.floor( Math.random() * max + min );
+// }
+
+// function getItem() {
+//   var width = randomInt( 150, 400 );
+//   var height = randomInt( 150, 250 );
+//   var item = '<div class="item">'+
+//     '<img src="http://lorempixel.com/' + 
+//       width + '/' + height + '/nature" /></div>';
+//   return item;
+// }
+
+// function getItems() {
+//   var items = '';
+//   for ( var i=0; i < 12; i++ ) {
+//     items += getItem();
+//   }
+//   // return jQuery object
+//   return $( items );
+// }
